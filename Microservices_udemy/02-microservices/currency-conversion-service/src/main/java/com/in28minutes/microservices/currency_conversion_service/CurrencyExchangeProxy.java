77deb8eb75f77cca6236496a48ddf05cd69e25e5
saikumar padamati <saikumarpadamati@saikumars-MacBook-Pro.local>
2024-06-03 13:40:30 -0400

    deleting on local

--- Microservices_udemy/02-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyExchangeProxy.java
+++ Microservices_udemy/02-microservices/currency-conversion-service/src/main/java/com/in28minutes/microservices/currency_conversion_service/CurrencyExchangeProxy.java
@@ -1,2 +1,14 @@
-package com.in28minutes.microservices.currency_conversion_service;public interface CurrencyExchangeProxy {
-}
+package com.in28minutes.microservices.currency_conversion_service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//@FeignClient(name="currency-exchange" , url="localhost:8000")  //currency conversion lo currency exchange
+@FeignClient(name="currency-exchange") //once we register with eureka, we no longer need to hardcode url and this automatically loadbalances
+public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+
+    }
