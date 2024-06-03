dfe74f3f2b128a9ed06abd6f7ab3ee62f1a1617d
saikumar padamati <saikumarpadamati@saikumars-MacBook-Pro.local>
2024-06-03 13:39:38 -0400

    deleting on local

--- Microservices_udemy/02-microservices/api-gateway/src/main/java/com/in28minutes/microservices/api_gateway/ApiGatewayConfiguration.java
+++ Microservices_udemy/02-microservices/api-gateway/src/main/java/com/in28minutes/microservices/api_gateway/ApiGatewayConfiguration.java
@@ -1,2 +1,42 @@
-package com.in28minutes.microservices.api_gateway;public class ApiGatewayConfiguration {
+package com.in28minutes.microservices.api_gateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder){
+        Function<PredicateSpec, Buildable<Route>> routeFunction
+                =p->p.path("/get")
+                .filters(f ->
+                        f.addRequestHeader("MyHeader","MyURI")
+                         .addRequestParameter("Param","MyValue"))
+                .uri("http://httpbin.org:80"); //redirecting to this uri if we go to "/get"
+        return  builder.routes()
+                .route(routeFunction)  //adding route function
+                .route(p->p.path("/currency-exchange/**")
+                           .uri("lb://currency-exchange"))//load balancing and redirecting
+                .route(p->p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))//load balancing and redirecting
+                .route(p->p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))//load balancing and redirecting
+                .route(p->p.path("/currency-conversion-new/**")
+                        .filters(f->f.rewritePath(
+                                "/currency-conversion-new/(?<segment>.*)",
+                                      "/currency-conversion-new/${segment}"))
+                        .uri("lb://currency-conversion"))//load balancing and redirecting
+
+                .build();
+    }
+
+
 }
--- Microservices_udemy/02-microservices/api-gateway/src/main/java/com/in28minutes/microservices/api_gateway/LoggingFiler.java
+++ Microservices_udemy/02-microservices/api-gateway/src/main/java/com/in28minutes/microservices/api_gateway/LoggingFiler.java
@@ -1,2 +1,19 @@
-package com.in28minutes.microservices.api_gateway;public class LoggingFiler {
+package com.in28minutes.microservices.api_gateway;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import org.slf4j.Logger;
+@Component
+public class LoggingFiler implements GlobalFilter {
+    private Logger logger= LoggerFactory.getLogger((LoggingFiler.class));
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the request received -> {}",exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
 }
